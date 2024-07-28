a9cb768f7b4d9d207cb2097a20b54c2c1eea3723
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2024-07-28 16:58:54 -0400

    Add break points such as for legend values for more accurate map

--- R/getrelief.R
+++ R/getrelief.R
@@ -1,28 +1,42 @@
-# library(climatools)
-# library(terra)
-# dem <- rast('C:/a/geo/dem/SRTM250m/usa/w001001.adf')
-#
-# dem1 <-  reproject(dem=dem, rs = 250, w=500000, h=500000, lat=47, lon=-122)
-# plot(dem1)
-#
-# dem2 <-  reproject(dem=dem, rs = 250, w=500000, h=500000, lat=44, lon=-85)
-# plot(dem2)
-
-
 #' Get topographic relief
 #'
 #' @param dm Digital elevation model (terra raster) with projected coordinates.
 #' @param r1 Minimum focal radius for estimating relief (only focal radius when n-0).
 #' @param r2 Maximum focal radius for estimating relief.
 #' @param n Number of segments between maximum and minimum radii to estimate relief.
-#' @param s Slope for which relief is estimated (Default is 0.1 for a 10% slope). Should be a ratio of vertical units to horizontal units).
-#' @param p precision, from low to exact, with higher levels of precision requiring more processing time.
+#' @param s Slope for which relief is estimated (Default is 0.1 for a 10\% slope). Should be a ratio of vertical units to horizontal units).
+#' @param p Precision from low to exact with higher levels of precision requiring more processing time.
+#' @param breaks Optional vector of relief thresholds to specifically calculate a radius rather than to depend on interpolation among a regular interval between maximum and minimum radii.
 #'
 #' @return Topographic relief of either a fixed radius or for a fixed slope for a range of radii.
+#'
 #' @export
 #'
-#' @examples
-getrelief <- function(dm, r1, r2, n=0, s=0.1, p=c('low', 'medium', 'high','exact')){
+#' @examples library(terra)
+#' #create fake raster
+#' x <- (1:10)/10-0.5
+#' y <- x
+#' df <- merge(x,y)
+#' df$z <- 1000/((df$x-0.25)^2+(df$y-0.40)^2+1)^2
+#' set.seed(1)
+#' df$z <- df$z +rnorm(10*10, sd = 100)
+#' df$x <- df$x*10 -85
+#' df$y <- df$y*10 +20
+#' dem <- rast(cbind(x=df$x,y=df$y,z=df$z), type="xyz", crs='EPSG:4326')
+#' dem <- reproject(dem=dem, rs = 250)
+#' plot(dem)
+#' #get relief with a 10% slope using 10 intervals between focal radii and default low precision.
+#' rng <- getrelief(dem, r1=1000, r2=100000, s=0.1, n=10)
+#' #get relief with a 25% slope using 10 intervals between focal radii and default low precision.
+#' rng <- getrelief(dem, r1=1000, r2=100000, s=0.25, n=10)
+#' plot(rng)
+#' #get 10% relief using specific break points of vertical relief and medium precision.
+#' rng <- getrelief(dem, r1=1000, r2=100000, s=0.1, n=1, p='medium', breaks = c(300, 1000, 2500, 5000))
+#' plot(rng)
+#' #get relief with a fixed radius of 2000 meters.
+#' rng <- getrelief(dem, r1=2000, s=0.1, n=0)
+#' plot(rng)
+getrelief <- function(dm, r1, r2, n=0, s=0.1, p=c('low', 'medium', 'high','exact'), breaks=NA){
   require(terra)
   #p is for precision options
   p=p[1]
@@ -35,6 +49,9 @@ getrelief <- function(dm, r1, r2, n=0, s=0.1, p=c('low', 'medium', 'high','exact
     d = log(r2) -  log(r1)
     ser <- n:0
     rx <- exp(log(r1)+d/n*ser)
+    if(!is.na(breaks[1])){
+    rx <- unique(sort(c(breaks/s,rx), decreasing=T))}
+
     for(i in 1:length(rx)){#i=2
       xmax <- focalmax(dm, rx[i]/2, p=p)
       xmin <- focalmin(dm, rx[i]/2, p=p)
