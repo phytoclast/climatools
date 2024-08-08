fcf1ed9c5c70afff1baddb4836b8cc304dbde232
Greg Schmidt <greg.schmidt@mi.usda.gov>
2024-08-08 19:24:13 -0400

    Update newprojections.R

--- data_raw/newprojections.R
+++ data_raw/newprojections.R
@@ -1,4 +1,5 @@
-
+library(sf)
+library(terra)
 scf <- 0.9996
 feast <- 0
 fnorth <- 0
@@ -10,34 +11,61 @@ lon1 <- NA
 lat2 <- lat + 8
 lon2 <- lon + 3
 
+setProjection(prj = 'cone.equaldistant', datum = 3, unit = 3, lat = lat, lon = lon)
+setProjection( lat = lat, lon = lon)
+
+
+
 
 
 setProjection <- function(prj = c('point.equalarea','point.equaldistant','conformal.point',
-                                    'cylinder.equalarea','cylinder.equaldistant','conformal.cylindric',
-                                    'cone.equalarea','cone.equaldistant','conformal.conic',
-                                    'conformal.transverse','conformal.oblique','geographic'),
+                                  'cylinder.equalarea','cylinder.equaldistant','conformal.cylindric',
+                                  'cone.equalarea','cone.equaldistant','conformal.conic',
+                                  'conformal.transverse','conformal.oblique','geographic'),
                           datum = c('WGS84','NAD83','NAD27'),unit=c('m','ft','usft'),
                           lat=NA, lon=NA, lat2=NA,lon2=NA,feast=0,fnorth=0,orglat=NA){
+  prj <- prj[1];datum <- datum[1];unit <- unit[1]
+  #alternative numeric parameter assigned to text
+  if(is.numeric(prj)){
+    preprj <- c('point.equalarea','point.equaldistant','conformal.point',
+                'cylinder.equalarea','cylinder.equaldistant','conformal.cylindric',
+                'cone.equalarea','cone.equaldistant','conformal.conic',
+                'conformal.transverse','conformal.oblique','geographic')
+    prj <- preprj[prj]
+  }
+  if(!is.numeric(datum)){
+    predatum <- c('WGS84','NAD83','NAD27')
+    datum <- which(predatum %in% datum)
+  }
+  if(!is.numeric(unit)){
+    preunit <- c('m','ft','usft')
+    unit <- which(preunit %in% unit)
+  }
 
-begin <- 'PROJCS["Custom",'
+  da=datum
 
-datums <- c(
-  #WGS 84
-  'GEOGCS["WGS 84",
+  un <- unit
+
+
+  begin <- 'PROJCS["Custom",'
+
+  datums <- c(
+    #WGS 84
+    'GEOGCS["WGS 84",
         DATUM["WGS_1984",
             SPHEROID["WGS 84",6378137,298.257223563]],
         PRIMEM["Greenwich",0],
         UNIT["Degree",0.0174532925199433]],',
-  #NAD83
-  'BASEGEOGCRS["NAD83",
+    #NAD83
+    'BASEGEOGCRS["NAD83",
         DATUM["North American Datum 1983",
             ELLIPSOID["GRS 1980",6378137,298.257222101,
                 LENGTHUNIT["metre",1]]],
         PRIMEM["Greenwich",0,
             ANGLEUNIT["degree",0.0174532925199433]],
         ID["EPSG",4269]],',
-  #NAD27
-  'BASEGEOGCRS["NAD27",
+    #NAD27
+    'BASEGEOGCRS["NAD27",
             DATUM["North American Datum 1927",
                   ELLIPSOID["Clarke 1866",6378206.4,294.978698213898,
                             LENGTHUNIT["metre",1]]],
@@ -47,312 +75,141 @@ datums <- c(
   )
 
 
-units <- c('UNIT["metre",1,AUTHORITY["EPSG","9001"]]',
-           'UNIT["foot",0.3048, AUTHORITY["EPSG","9002"]]',
-           'UNIT["US survey foot",0.304800609601219,AUTHORITY["EPSG","9003"]]')
+  unitss <- c('UNIT["metre",1,AUTHORITY["EPSG","9001"]]',
+             'UNIT["foot",0.3048, AUTHORITY["EPSG","9002"]]',
+             'UNIT["US survey foot",0.304800609601219,AUTHORITY["EPSG","9003"]]')
 
 
 
-axises <- ',AXIS["Easting",EAST], AXIS["Northing",NORTH]]]'
+  axises <- ',AXIS["Easting",EAST], AXIS["Northing",NORTH]]]'
 
 
-projections <- c(
-  #1.azimutal 1,2
-  'PROJECTION["Lambert_Azimuthal_Equal_Area"],',
-  'PROJECTION["Azimuthal_Equidistant"],',
-  #2.stereographic 3
-  'PROJECTION["Stereographic"],',
-  #3.cylindric 4,5
-  'PROJECTION["Cylindrical_Equal_Area"],',
-  'PROJECTION["Equirectangular"],',
-  #4.mercator 6
-  'PROJECTION["Mercator_2SP"],',
-  #5.conic 7,8
-  'PROJECTION["Albers_Conic_Equal_Area"],',
-  'PROJECTION["Equidistant_Conic"],',
-  #6.conformal conic 9
-  'PROJECTION["Lambert_Conformal_Conic_2SP"],',
-  #7.transverse 10
-  'PROJECTION["Transverse_Mercator"],',
-  #8.oblique 11
-  'PROJECTION["Hotine_Oblique_Mercator_Two_Point_Natural_Origin"],'
-)
+  projections <- c(
+    #1.azimutal 1,2
+    'PROJECTION["Lambert_Azimuthal_Equal_Area"],',
+    'PROJECTION["Azimuthal_Equidistant"],',
+    #2.stereographic 3
+    'PROJECTION["Stereographic"],',
+    #3.cylindric 4,5
+    'PROJECTION["Cylindrical_Equal_Area"],',
+    'PROJECTION["Equirectangular"],',
+    #4.mercator 6
+    'PROJECTION["Mercator_2SP"],',
+    #5.conic 7,8
+    'PROJECTION["Albers_Conic_Equal_Area"],',
+    'PROJECTION["Equidistant_Conic"],',
+    #6.conformal conic 9
+    'PROJECTION["Lambert_Conformal_Conic_2SP"],',
+    #7.transverse 10
+    'PROJECTION["Transverse_Mercator"],',
+    #8.oblique 11
+    'PROJECTION["Hotine_Oblique_Mercator_Two_Point_Natural_Origin"],'
+  )
 
 
 
-if(is.na(lat1)){lat1 <- lat - 8}
-if(is.na(lon1)){lon1 <- lon - 3}
-if(is.na(lat2)){lat2 <- lat + 8}
-if(is.na(lon2)){lon2 <- lon + 3}
-if(is.na(orglat)){orglat <- lat}
+  if(is.na(lat1)){lat1 <- lat - 8}
+  if(is.na(lon1)){lon1 <- lon - 3}
+  if(is.na(lat2)){lat2 <- lat + 8}
+  if(is.na(lon2)){lon2 <- lon + 3}
+  if(is.na(orglat)){orglat <- lat}
 
 
-azcentlat <- paste0('PARAMETER["latitude_of_center",',lat,'],')
-azcentlon <- paste0('PARAMETER["longitude_of_center",',lon,'],')
-parfeast <- paste0('PARAMETER["false_easting",',feast,'],')
-parfnorth <- paste0('PARAMETER["false_northing",',fnorth,'],')
-parscf <- paste0('PARAMETER["scale_factor",',scf,'],')
+  azcentlat <- paste0('PARAMETER["latitude_of_center",',lat,'],')
+  azcentlon <- paste0('PARAMETER["longitude_of_center",',lon,'],')
+  parfeast <- paste0('PARAMETER["false_easting",',feast,'],')
+  parfnorth <- paste0('PARAMETER["false_northing",',fnorth,'],')
+  parscf <- paste0('PARAMETER["scale_factor",',scf,'],')
 
 
-coniclat1 <- paste0('PARAMETER["standard_parallel_1",',lat1,'],')
-coniclat2 <- paste0('PARAMETER["standard_parallel_2",',lat2,'],')
-coniclat0 <- paste0('PARAMETER["latitude_of_origin",',orglat,'],')
-coniclon <- paste0('PARAMETER["central_meridian",',lon,'],')
+  coniclat1 <- paste0('PARAMETER["standard_parallel_1",',lat1,'],')
+  coniclat2 <- paste0('PARAMETER["standard_parallel_2",',lat2,'],')
+  coniclat0 <- paste0('PARAMETER["latitude_of_origin",',orglat,'],')
+  coniclon <- paste0('PARAMETER["central_meridian",',lon,'],')
 
-oblilat1 <- paste0('PARAMETER["latitude_of_point_1",',lat1,'],')
-oblilon1 <- paste0('PARAMETER["longitude_of_point_1",',lon1,'],')
-oblilat2 <- paste0('PARAMETER["latitude_of_point_2",',lat2,'],')
-oblilon2 <- paste0('PARAMETER["longitude_of_point_2",',lon2,'],')
-
-
-ifaz <- paste0(azcentlat, azcentlon)
-ifcyl <- paste0(coniclat1, coniclon)
-ifcon <- paste0(azcentlat, azcentlon, coniclat1, coniclat2)
-ifconf <- paste0(coniclat0, coniclon, coniclat1, coniclat2)
-ifutm <- paste0(coniclat0, coniclon, parscf)
-ifobl <- paste0(azcentlat, oblilat1, oblilon1, oblilat2, oblilon2, parscf)
-ifster <- paste0(coniclat0, coniclon)
-pickparameters <- c(ifaz, ifcyl, ifcon, ifconf, ifutm, ifobl, ifster)
-
-da <- 1
-sh <- 1
-eq <- 1
-va <- 1
-un <- 1
+  oblilat1 <- paste0('PARAMETER["latitude_of_point_1",',lat1,'],')
+  oblilon1 <- paste0('PARAMETER["longitude_of_point_1",',lon1,'],')
+  oblilat2 <- paste0('PARAMETER["latitude_of_point_2",',lat2,'],')
+  oblilon2 <- paste0('PARAMETER["longitude_of_point_2",',lon2,'],')
 
 
-if(prj %in% 'point.equalarea'){
-  prochoice <- 1
-  pramchoice <- 1
-}
-if(prj %in% 'point.equaldistant'){
-  prochoice <- 2
-  pramchoice <- 1
-}
-if(prj %in% 'conformal.point'){
-  prochoice <- 3
-  pramchoice <- 2
-}
-
-if(prj %in% 'cylinder.equalarea'){
-  prochoice <- 4
-  pramchoice <- 3
-}
-if(prj %in% 'cylinder.equaldistant'){
-  prochoice <- 5
-  pramchoice <- 3
-}
-if(prj %in% 'conformal.cylindric'){
-  prochoice <- 6
-  pramchoice <- 4
-}
-if(prj %in% 'cone.equalarea'){
-  prochoice <- 7
-  pramchoice <- 5
-}
-if(prj %in% 'cone.equaldistant'){
-  prochoice <- 8
-  pramchoice <- 5
-}
-if(prj %in% 'conformal.conic'){
-  prochoice <- 9
-  pramchoice <- 6
-}
-
-if(prj %in% 'conformal.transverse'){
-  prochoice <- 10
-  pramchoice <- 7
-}
-if(prj %in% 'conformal.oblique'){
-  prochoice <- 11
-  pramchoice <- 8
-}
+  ifaz <- paste0(azcentlat, azcentlon)
+  ifcyl <- paste0(coniclat1, coniclon)
+  ifcon <- paste0(azcentlat, azcentlon, coniclat1, coniclat2)
+  ifconf <- paste0(coniclat0, coniclon, coniclat1, coniclat2)
+  ifutm <- paste0(coniclat0, coniclon, parscf)
+  ifobl <- paste0(azcentlat, oblilat1, oblilon1, oblilat2, oblilon2, parscf)
+  ifster <- paste0(coniclat0, coniclon)
+  pickparameters <- c(ifaz, ifcyl, ifcon, ifconf, ifutm, ifobl, ifster)
 
 
-assembly <- paste0(begin,
-               datums[da],
-               projections[prochoice],
-               pickparameters[pramchoice],
-               parfeast, parfnorth, units[un],axises)
 
+  if(prj %in% 'point.equalarea'){
+    prochoice <- 1
+    pramchoice <- 1
+  }
+  if(prj %in% 'point.equaldistant'){
+    prochoice <- 2
+    pramchoice <- 1
+  }
+  if(prj %in% 'conformal.point'){
+    prochoice <- 3
+    pramchoice <- 2
+  }
+
+  if(prj %in% 'cylinder.equalarea'){
+    prochoice <- 4
+    pramchoice <- 3
+  }
+  if(prj %in% 'cylinder.equaldistant'){
+    prochoice <- 5
+    pramchoice <- 3
+  }
+  if(prj %in% 'conformal.cylindric'){
+    prochoice <- 6
+    pramchoice <- 4
+  }
+  if(prj %in% 'cone.equalarea'){
+    prochoice <- 7
+    pramchoice <- 5
+  }
+  if(prj %in% 'cone.equaldistant'){
+    prochoice <- 8
+    pramchoice <- 5
+  }
+  if(prj %in% 'conformal.conic'){
+    prochoice <- 9
+    pramchoice <- 6
+  }
+
+  if(prj %in% 'conformal.transverse'){
+    prochoice <- 10
+    pramchoice <- 7
+  }
+  if(prj %in% 'conformal.oblique'){
+    prochoice <- 11
+    pramchoice <- 8
+  }
+
+
+  assembly <- paste0(begin,
+                     datums[da],
+                     projections[prochoice],
+                     pickparameters[pramchoice],
+                     parfeast, parfnorth, unitss[un],axises)
+
+  return(assembly)
 
 }
 
 
-#WGS 84
-st_crs('EPSG:4326')
-'GEOGCS["WGS 84",
-        DATUM["WGS_1984",
-            SPHEROID["WGS 84",6378137,298.257223563]],
-        PRIMEM["Greenwich",0],
-        UNIT["Degree",0.0174532925199433]],'
-#NAD83
-st_crs('EPSG:4269')
-'BASEGEOGCRS["NAD83",
-        DATUM["North American Datum 1983",
-            ELLIPSOID["GRS 1980",6378137,298.257222101,
-                LENGTHUNIT["metre",1]]],
-        PRIMEM["Greenwich",0,
-            ANGLEUNIT["degree",0.0174532925199433]],
-        ID["EPSG",4269]],'
-#NAD27
-st_crs('EPSG:4267')
-'BASEGEOGCRS["NAD27",
-            DATUM["North American Datum 1927",
-                  ELLIPSOID["Clarke 1866",6378206.4,294.978698213898,
-                            LENGTHUNIT["metre",1]]],
-            PRIMEM["Greenwich",0,
-                   ANGLEUNIT["degree",0.0174532925199433]],
-            ID["EPSG",4267]],'
-st_crs('EPSG:3857')
-st_crs('EPSG:3079')
-st_crs('EPSG:6202')
-st_crs('EPSG:2251')
-st_crs('ESRI:102008')
-st_crs('ESRI:102010')
-st_crs('EPSG:10598')
-
-#equalarea
-#equidistant
-#conformal (equal angle)
-
-#azimuthal equalarea
-'PROJECTION["Lambert_Azimuthal_Equal_Area"],
-        PARAMETER["latitude_of_center",52],
-        PARAMETER["longitude_of_center",10],
-        PARAMETER["false_easting",4321000],
-        PARAMETER["false_northing",3210000],
-        UNIT["metre",1,
-            AUTHORITY["EPSG","9001"]],
-        AXIS["Easting",EAST],
-        AXIS["Northing",NORTH]]'
-
-#cylindric equalarea
-'PROJECTION["Cylindrical_Equal_Area"],
-PARAMETER["standard_parallel_1",0],
-PARAMETER["central_meridian",0],
-PARAMETER["false_easting",0],
-PARAMETER["false_northing",0],
-UNIT["metre",1,
-     AUTHORITY["EPSG","9001"]],
-AXIS["Easting",EAST],
-AXIS["Northing",NORTH]]'
-
-#conic equalarea
-'PROJECTION["Albers_Conic_Equal_Area"],
-    PARAMETER["latitude_of_center",23],
-    PARAMETER["longitude_of_center",-96],
-    PARAMETER["standard_parallel_1",29.5],
-    PARAMETER["standard_parallel_2",45.5],
-    PARAMETER["false_easting",0],
-    PARAMETER["false_northing",0],
-    UNIT["metre",1,
-        AUTHORITY["EPSG","9001"]],
-    AXIS["Easting",EAST],
-    AXIS["Northing",NORTH]]'
-
-#oblique equalarea
-
-
-#azimuthal equidistant
-'PROJECTION["Azimuthal_Equidistant"],
-    PARAMETER["latitude_of_center",90],
-    PARAMETER["longitude_of_center",0],
-    PARAMETER["false_easting",0],
-    PARAMETER["false_northing",0],
-    UNIT["metre",1,
-        AUTHORITY["EPSG","9001"]],
-    AXIS["Easting",EAST],
-    AXIS["Northing",NORTH]'
-
-#cylindric equidistant
-'PROJECTION["Equirectangular"],
-PARAMETER["standard_parallel_1",0],
-PARAMETER["central_meridian",0],
-PARAMETER["false_easting",0],
-PARAMETER["false_northing",0],
-UNIT["metre",1,
-     AUTHORITY["EPSG","9001"]],
-AXIS["Easting",EAST],
-AXIS["Northing",NORTH]]'
-
-#conic equidistant
-'PROJECTION["Equidistant_Conic"],
-PARAMETER["latitude_of_center",40],
-PARAMETER["longitude_of_center",-96],
-PARAMETER["standard_parallel_1",20],
-PARAMETER["standard_parallel_2",60],
-PARAMETER["false_easting",0],
-PARAMETER["false_northing",0],
-UNIT["metre",1,
-     AUTHORITY["EPSG","9001"]],
-AXIS["Easting",EAST],
-AXIS["Northing",NORTH]'
-
-#oblique equidistant
-
-#azimuthal conformal
-'PROJECTION["Polar_Stereographic"],
-PARAMETER["latitude_of_origin",90],
-PARAMETER["central_meridian",0],
-PARAMETER["scale_factor",1],
-PARAMETER["false_easting",0],
-PARAMETER["false_northing",0]'
-
-
-#cylindric conformal
-'PROJECTION["Mercator_2SP"],
-PARAMETER["standard_parallel_1",0],
-PARAMETER["central_meridian",0],
-PARAMETER["false_easting",0],
-PARAMETER["false_northing",0],
-UNIT["metre",1,
-     AUTHORITY["EPSG","9001"]],
-AXIS["Easting",EAST],
-AXIS["Northing",NORTH]]'
-
-#conic conformal
-'PROJECTION["Lambert_Conformal_Conic_2SP"],
-PARAMETER["latitude_of_origin",40],
-PARAMETER["central_meridian",-96],
-PARAMETER["standard_parallel_1",20],
-PARAMETER["standard_parallel_2",60],
-PARAMETER["false_easting",0],
-PARAMETER["false_northing",0],
-UNIT["metre",1,
-     AUTHORITY["EPSG","9001"]],
-AXIS["Easting",EAST],
-AXIS["Northing",NORTH]]'
-
-#transverse conformal
-'    PROJECTION["Transverse_Mercator"],
-    PARAMETER["latitude_of_origin",0],
-    PARAMETER["central_meridian",15],
-    PARAMETER["scale_factor",0.9996],
-    PARAMETER["false_easting",500000],
-    PARAMETER["false_northing",0],
-    UNIT["metre",1,
-        AUTHORITY["EPSG","9001"]],
-    AXIS["Easting",EAST],
-    AXIS["Northing",NORTH]]'
-
-#oblique conformal
-'PROJECTION["Hotine_Oblique_Mercator_Two_Point_Natural_Origin"],
-    PARAMETER["latitude_of_center",40],
-    PARAMETER["latitude_of_point_1",0],
-    PARAMETER["longitude_of_point_1",0],
-    PARAMETER["latitude_of_point_2",60],
-    PARAMETER["longitude_of_point_2",60],
-    PARAMETER["scale_factor",1],
-    PARAMETER["false_easting",0],
-    PARAMETER["false_northing",0],
-'
 
 
 
 
 
-reproject <- function(dem, lat=NA, lon=NA, rs = NA,  h = NA, w = NA){
+
+reproject <- function(dem, lat=NA, lon=NA, rs = NA,  h = NA, w = NA, prj=NA, datum=NA, unit=NA){
   require(terra)
   require(sf)
 
@@ -376,18 +233,7 @@ reproject <- function(dem, lat=NA, lon=NA, rs = NA,  h = NA, w = NA){
     lon <- pt.trans[,1]
   }
 
-  wkt.new <- paste0('PROJCS["Centered Equal Area",
-    GEOGCS["WGS 84",
-        DATUM["WGS_1984",
-            SPHEROID["WGS 84",6378137,298.257223563]],
-        PRIMEM["Greenwich",0],
-        UNIT["Degree",0.0174532925199433]],
-    PROJECTION["Lambert_Azimuthal_Equal_Area"],
-    PARAMETER["latitude_of_center",',lat,'],
-    PARAMETER["longitude_of_center",',lon,'],
-    PARAMETER["false_easting",0],
-    PARAMETER["false_northing",0],
-    UNIT["metre",1]]')
+  wkt.new <- setProjection(prj=1, datum=1,unit=1, lat=lat, lon=lon)
 
   #find units and resolution of current projection
   ishfeet <- (grepl('unit.*"foot".*vertcrs',tolower(st_crs(dem))) | !grepl('vertcrs',tolower(st_crs(dem))) & grepl('unit.*"foot"',tolower(st_crs(dem))))
