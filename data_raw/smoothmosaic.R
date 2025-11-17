22b41d6c373f12723ffeb6c77d269cc927915895
Greg Schmidt <gregoryjohnschmidt@gmail.com>
2025-11-17 16:55:58 -0500

    Update smoothmosaic.R

--- data_raw/smoothmosaic.R
+++ data_raw/smoothmosaic.R
@@ -24,28 +24,28 @@ smoothmosaic <- function(r1,r2){
     r <- merge(r1,r2)
     return(r)
   }
-  
+
   #determine relationships to feed into appropriate subroutines
   r2Xinside <- er1[1]  < er2[1] & er1[2]  > er2[2]
   r1Xinside <- er2[1]  < er1[1] & er2[2]  > er1[2]
   r2Yinside <- er1[3]  < er2[3] & er1[4]  > er2[4]
   r1Yinside <- er2[3]  < er1[3] & er2[4]  > er1[4]
   anyinside <- r1Yinside | r2Yinside | r1Xinside | r2Xinside
-  
-  
-  tallign  <- er1[4] == er2[4] 
-  lallign  <- er1[1] == er2[1] 
-  ballign  <- er1[3] == er2[3] 
+
+
+  tallign  <- er1[4] == er2[4]
+  lallign  <- er1[1] == er2[1]
+  ballign  <- er1[3] == er2[3]
   rallign  <- er1[2] == er2[2]
-  
-  yallign  <- tallign & ballign 
+
+  yallign  <- tallign & ballign
   xallign  <- lallign & rallign
-  
+
   #simple one sided relationships
-  r1left   <- er1[2] < er2[2] 
-  r1right  <- er1[1] > er2[1] 
-  r1top    <- er1[3] > er2[3] 
-  r1bottom <- er1[4] < er2[4] 
+  r1left   <- er1[2] < er2[2]
+  r1right  <- er1[1] > er2[1]
+  r1top    <- er1[3] > er2[3]
+  r1bottom <- er1[4] < er2[4]
   #corner relationships
   r1ne <- r1right & r1top
   r1se <- r1right & r1bottom
@@ -56,7 +56,7 @@ smoothmosaic <- function(r1,r2){
   r1r  <- r1right & yallign
   r1t  <- r1top & xallign
   r1b  <- r1bottom & xallign
-  
+
   #raster completely inside other raster
   xyinside <- (er1[1] > er2[1] & er1[2] < er2[2] &
                  er1[3] > er2[3] & er1[4] < er2[4])|
@@ -64,7 +64,7 @@ smoothmosaic <- function(r1,r2){
        er1[3] < er2[3] & er1[4] > er2[4])
   #full band or strip overlap
   yband <- xallign
-  xband <- yallign 
+  xband <- yallign
   #band surpasses on one side
   xbandr <- lallign & !yallign  & (er1[2] > er2[2] & r1Yinside| er1[2] < er2[2] & r2Yinside)
   xbandl <- rallign & !yallign  & (er1[1] < er2[1] & r1Yinside| er1[1] > er2[1] & r2Yinside)
@@ -73,13 +73,13 @@ smoothmosaic <- function(r1,r2){
   #incomplete tongue overlap
   xtongr <- !yallign  & (er1[2] > er2[2] & r1Yinside| er1[2] < er2[2] & r2Yinside)&
     (er1[1] > er2[1] & r1Yinside| er1[1] < er2[1] & r2Yinside)
-  
+
   xtongl <- !yallign  & (er1[1] < er2[1] & r1Yinside| er1[1] > er2[1] & r2Yinside)&
     (er1[2] < er2[2] & r1Yinside| er1[2] > er2[2] & r2Yinside)
-  
+
   ytongt <- !xallign  & (er1[4] > er2[4] & r1Xinside| er1[4] < er2[4] & r2Xinside)&
     (er1[3] > er2[3] & r1Xinside| er1[3] < er2[3] & r2Xinside)
-  
+
   ytongb <- !xallign  & (er1[3] < er2[3] & r1Xinside| er1[3] > er2[3] & r2Xinside)&
     (er1[4] < er2[4] & r1Xinside| er1[4] > er2[4] & r2Xinside)
   #cross
@@ -103,18 +103,20 @@ smoothmosaic <- function(r1,r2){
   int <- lallign  & rallign & !ballign & tallign
   inb <- lallign  & rallign & ballign & !tallign
   anyinner <- inse|insw|inne|innw|inr|inl|int|inb
-  
+
   #perform intersect
   ei <- terra::intersect(ext(r1),ext(r2))
   r1i <- r1 |> crop(ei)
   r2i <- r2 |> crop(ei)
-  
+  r1internal <- !(er1[1] < ei[1] | er1[2] > ei[2] | er1[3] < ei[3] | er1[4] > ei[4])
+  r2internal <- !(er2[1] < ei[1] | er2[2] > ei[2] | er2[3] < ei[3] | er2[4] > ei[4])
+
   #create mask
   msk <- r1i*0+1
-  
+
   #full intersect routines using entire gradient of overlapping region
   if(!anyinside & !anyinner){
-    
+
     l.full0 <- function(){
       values(msk) <- rep(seq(1, 0, length.out = ncol(msk)), times = nrow(msk))
       return(msk)
@@ -143,7 +145,7 @@ smoothmosaic <- function(r1,r2){
     if(r1b){
       values(msk) <- rep(seq(0, 1, length.out = nrow(msk)),each = ncol(msk))
     }
-    
+
     if(r1ne){
       r.full <- r.full0()
       t.full <- t.full0()
@@ -151,7 +153,7 @@ smoothmosaic <- function(r1,r2){
       pmsk2 <- (1-r.full)*(1-t.full)
       msk <- (pmsk1+0.01)/(pmsk1+pmsk2+0.02)
     }
-    
+
     if(r1se){
       r.full <- r.full0()
       b.full <- b.full0()
@@ -159,7 +161,7 @@ smoothmosaic <- function(r1,r2){
       pmsk2 <- (1-r.full)*(1-b.full)
       msk <- (pmsk1+0.01)/(pmsk1+pmsk2+0.02)
     }
-    
+
     if(r1sw){
       l.full <- l.full0()
       b.full <- b.full0()
@@ -167,7 +169,7 @@ smoothmosaic <- function(r1,r2){
       pmsk2 <- (1-l.full)*(1-b.full)
       msk <- (pmsk1+0.01)/(pmsk1+pmsk2+0.02)
     }
-    
+
     if(r1nw){
       l.full <- l.full0()
       t.full <- t.full0()
@@ -175,41 +177,41 @@ smoothmosaic <- function(r1,r2){
       pmsk2 <- (1-l.full)*(1-t.full)
       msk <- (pmsk1+0.01)/(pmsk1+pmsk2+0.02)
     }
-    
-  
-    
+
+
+
   }else{
-    
+
     mwidth = 1/4
     bbrk <- ei[3] + (ei[4]-ei[3])*mwidth
     tbrk <- ei[3] + (ei[4]-ei[3])*(1-mwidth)
     lbrk <- ei[1] + (ei[2]-ei[1])*mwidth
     rbrk <- ei[1] + (ei[2]-ei[1])*(1-mwidth)
-    
+
     bflank <- crop(msk, ext(ei[1],ei[2],ei[3],bbrk))
     bcore <- bflank
     ycore <- crop(msk, ext(ei[1],ei[2],bbrk,tbrk))
     tflank <- crop(msk, ext(ei[1],ei[2],tbrk,ei[4]))
     tcore <- tflank
-    
+
     lflank <- crop(msk, ext(ei[1],lbrk,ei[3],ei[4]))
     lcore <- lflank
     xcore <- crop(msk, ext(lbrk,rbrk,ei[3],ei[4]))
     rflank <- crop(msk, ext(rbrk,ei[2],ei[3],ei[4]))
     rcore <- rflank
-    
+
     values(lflank) <- rep(seq(0, 1, length.out = ncol(lflank)), times = nrow(lflank))
     values(xcore) <- 1
     values(rflank) <- rep(seq(1, 0, length.out = ncol(rflank)), times = nrow(rflank))
     values(bflank) <- rep(seq(1, 0, length.out = nrow(bflank)), each = ncol(bflank))
     values(ycore) <- 1
     values(tflank) <- rep(seq(0, 1, length.out = nrow(tflank)), each = ncol(tflank))
-    
+
     values(lcore) <- 1
     values(rcore) <- 1
     values(bcore) <- 1
     values(tcore) <- 1
-    
+
     y.msk0 <- function(){
       y.msk <- merge(bflank,ycore,tflank)
       return(y.msk)}
@@ -228,10 +230,10 @@ smoothmosaic <- function(r1,r2){
     b.msk0 <- function(){
       b.msk <- merge(bflank,ycore,tcore)
       return(b.msk)}
-    
-    
-    
-    
+
+
+
+
     if(xyinside){msk <- min(x.msk0(),y.msk0())}
     #full band or strip overlap
     if(yband){msk <- y.msk0()
@@ -240,7 +242,7 @@ smoothmosaic <- function(r1,r2){
     if(r2Xinside){msk <- 1-msk}}
     #band surpasses on one side
     if(xbandr){msk <- min((y.msk0()+0.01)/(r.msk0()+0.01),1)
-    if(r2Yinside){msk <- 1-msk}}  
+    if(r2Yinside){msk <- 1-msk}}
     if(xbandl){msk <- min((y.msk0()+0.01)/(l.msk0()+0.01),1)
     if(r2Yinside){msk <- 1-msk}}
     if(ybandt){msk <- min((x.msk0()+0.01)/(t.msk0()+0.01),1)
@@ -283,9 +285,9 @@ smoothmosaic <- function(r1,r2){
     if(er1[3] < er2[3]){msk <- 1-msk}}
     if(inb){msk <- b.msk0()
     if(er1[4] > er2[4]){msk <- 1-msk}}
-    
-    
-    
+
+
+
     #cross
     if(xycross){
       y.msk <- y.msk0()
@@ -297,12 +299,12 @@ smoothmosaic <- function(r1,r2){
           msk <- 1-msk
         }
     }
-    
+
   }
-  
-  
+
+
     gmsk <- r1i*msk+r2i*(1-msk)
-  
+
   r <- merge(gmsk,r1,r2)
   return(r)
 }
@@ -321,6 +323,18 @@ plot(r)
 
 
 ##########################################
+
+r1 <- rast(xmin=20, xmax=80, ymin=0, ymax=80, res=1, vals=1, crs='EPSG:4326')
+r2 <- rast(xmin=0, xmax=80, ymin=20, ymax=80, res=1, vals=2, crs='EPSG:4326')
+
+ei <- terra::intersect(ext(r1),ext(r2))
+r1i <- r1 |> crop(ei)
+r2i <- r2 |> crop(ei)
+r1internal <- !(er1[1] < ei[1] | er1[2] > ei[2] | er1[3] < ei[3] | er1[4] > ei[4])
+r2internal <- !(er2[1] < ei[1] | er2[2] > ei[2] | er2[3] < ei[3] | er2[4] > ei[4])
+
+#create mask
+msk <- r1i*0+1
 
 
 l.full <- msk
@@ -404,7 +418,7 @@ r.msk <- merge(lcore,xcore,rflank)
 l.msk <- merge(lflank,xcore,rcore)
 t.msk <- merge(bcore,ycore,tflank)
 b.msk <- merge(bflank,ycore,tcore)
-
+plot(b.msk)
 msk <- min((y.msk+0.01)/(r.msk+0.01),1)
 msk <- min((y.msk+0.01)/(l.msk+0.01),1)
 msk <- min((x.msk+0.01)/(t.msk+0.01),1)
@@ -433,6 +447,21 @@ msk <- min((y.msk+0.01)/(x.msk+0.01),1)
 msk1 <- min((x.msk+0.01)/(y.msk+0.01),1)
 msk2 <- min((y.msk+0.01)/(x.msk+0.01),1)
 msk <- (msk1 + 1-msk2)/2
+plot(1-msk)
+
+#smooth cross
+xswale <- 1-(t.full*b.full)^0.5
+yswale <- 1-(l.full*r.full)^0.5
+msk1 <- min((yswale+0.01)/(xswale+0.01),1)
+msk2 <- min((xswale+0.01)/(yswale+0.01),1)
+msk <- (msk1 + 1-msk2)/2
+plot(msk)
+
+#smooth semicross
+msk1 <- min((r.full+0.01)/(t.full+0.01),1)
+msk2 <- min((t.full+0.01)/(r.full+0.01),1)
+
+msk <- (msk1 + 1-msk2)/2
 plot(msk)
 
 gmsk <- r1i*msk+r2i*(1-msk)
@@ -440,10 +469,15 @@ gmsk <- r1i*msk+r2i*(1-msk)
 r <- merge(gmsk,r1,r2)
 plot(r)
 
+
 msk <- x.msk
 msk <- y.msk
+#simple patch
 msk <- min(x.msk,y.msk)
-
+plot(msk>.9)
+#smoother patch
+msk <-(t.full*b.full*r.full*l.full/0.5^4)^0.5
+plot(msk>.5)
 
 msk <- min(y.msk,l.msk)
 
