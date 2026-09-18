30e25512db7a93ace70fb4e402f9682cda4c4931
Greg Schmidt <gregoryjohnschmidt@gmail.com>
2026-09-17 22:20:43 -0400

    Update modelbuilder.R

--- data_raw/modelbuilder.R
+++ data_raw/modelbuilder.R
@@ -9,27 +9,37 @@ plot(dem0)
 t1 <- enhanceRast(t0, dem0,  dem)
 plot(t1)
 
+library(sf)
+x <- thisgrid#; t1<-logp(t1)
+cropto <- c(-90, -75, 40, 55)
+cropfrom <- c(-82,-80, 45.5,49)
+
+modelfit <- function(x, elev=NULL, cropto, cropfrom=NULL, rotations=0){
+  t0 <- crop(x, cropto); names(t0) <- 't0'
+  xy0 <- makexyrast(t0,rotations)
+  if(!is.null(cropfrom)){
+    t00 <- crop(x, cropfrom)
+    t00 <- ifel(t00>0,NA,NA)
+    t0 <- merge(t00,t0, na.rm=FALSE); names(t0) <- 't0'
+  }
+  if(!is.null(elev)){
+    e0 <- project(elev, t0) |> crop(cropto)
+    u <- terra::linearUnits(t0)
+    u <- ifelse(u == 0, 10000000/90, u)
+    rs <- (res(e0)*u)[1]
+    emd <- focalmed(e0, rs*10)
+
+    erel <- e0-emd
+    erel <- ifel(erel > 0,erel,0)^0.5 - ifel(-erel > 0,-erel,0)^0.5; names(erel)<-'erel'
+    rss <- c(t0, e0, erel, xy0)
+  }else{
+    rss <- c(t0, xy0)
+
+  }
 
 
-c1 <- c(-152,-149.5, 62.6, 63.2)
-c2 <- c(-151.5,-150, 62.8, 63)
-t0a <- crop(t1, c1)
-t00 <- crop(t1, c2)
-t00 <- ifel(t00>0,NA,NA)
-t0 <- merge(t00,t0a, na.rm=FALSE); names(t0) <- 't0'
-plot(t0)
-e0 <- project(dem, t0)
-u <- terra::linearUnits(t0)
-u <- ifelse(u == 0, 10000000/90, u)
-rs <- (res(dem)*u)[1]
-emd <- focalmed(e0, rs*10)
 
-erel <- e0-emd
-erel <- ifel(erel > 0,erel,0)^0.5 - ifel(-erel > 0,-erel,0)^0.5; names(erel)<-'erel'
 
-rotations <- 10
-xy0 <- makexyrast(e0,rotations)
-rss <- c(t0, e0, erel, xy0)
 
 rssnames <- names(rss)
 
@@ -38,7 +48,7 @@ covars1 <- rssnames[2:5]
 covars2 <- rssnames[2:length(rssnames)]
 wts<- c(1,1,c(1:((rotations+1)*2))*0+1/(rotations+1))
 covars3 <- c(rssnames[2:5],'resids')
-df0 <- terra::spatSample(rss, 1500) |> subset(!is.na(t0))
+df0 <- terra::spatSample(rss, 15000) |> subset(!is.na(t0))
 dfsp <- st_as_sf(df0, coords = c(x='lon', y='lat'), crs=st_crs(t1))
 
 # f.glm <- as.formula(paste(paste(depvar,paste(paste(covars1, "+ I(",covars1,"^2)",collapse = " + ", sep = ""),""), sep = " ~ ")
@@ -68,18 +78,18 @@ rf <- ranger(f.rf,
              data=df0)
 resids <- terra::predict(rss, rf)
 resids <- focalmed(resids, rs*3); names(resids) <- 'resids'
-# plot(resids)
-# points(vect(dfsp))
 
 
 rss1 <- c(rss, resids)
 
-df1 <- terra::spatSample(rss1, 1500) |> subset(!is.na(t0))
+df1 <- terra::spatSample(rss1, 15000) |> subset(!is.na(t0))
 
 
 gm2 <- glm(f.glm2,
           family='gaussian',
           data=df1)
-summary(gm2)
-pred <- terra::predict(rss1, gm2) ; names(pred) <- 'pred'
-plot(pred-t0a)
+
+pred <- terra::predict(rss1, gm2) ; names(pred) <- 'pred'}
+return(pred)
+
+plot(pred)
